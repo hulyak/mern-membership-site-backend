31bef2a2e6b67c0c221c44db5d0364304904cf2f
hulya <hulyakarakayaa06@gmail.com>
2021-01-27 19:03:19 -0800

    implement creae group page and add it to database

--- src/db/index.js
+++ src/db/index.js
@@ -2,3 +2,4 @@ export { db } from './db';
 export { getAllGroups } from './getAllGroups';
 export { getUserGroups } from './getUserGroups';
 export { createJoinRequest } from './createJoinRequest'; 
+export { createGroup } from './createGroup';
--- src/routes/createGroupRoute.js
+++ src/routes/createGroupRoute.js
@@ -1,7 +1,19 @@
-export const createGroupRoute = {
-    method: 'post',
-    path: '/groups',
-    handler: async (req, res) => {
+import * as admin from 'firebase-admin';
+import { createGroup } from '../db';
 
+export const createGroupRoute = {
+  method: 'post',
+  path: '/groups',
+  handler: async (req, res) => {
+    const token = req.headers.authtoken;
+    const { name } = req.body; // get the name of the new group
+    if (!token) {
+      res
+        .status(401)
+        .json({ message: 'Must be authenticated to create a new group' });
     }
-}
+    const user = await admin.auth().verifyIdToken(token);
+    const newGroupId = await createGroup(name, user.user_id);
+    res.status(200).json({ newGroupId });
+  },
+};
