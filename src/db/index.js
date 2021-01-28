82c4f49f0b3ea366efa770274c33651d7c57beec
hulya <hulyakarakayaa06@gmail.com>
2021-01-27 18:19:23 -0800

    create a route and request to database for creating a request to join a group

--- src/db/index.js
+++ src/db/index.js
@@ -1,2 +1,4 @@
 export { db } from './db';
 export { getAllGroups } from './getAllGroups';
+export { getUserGroups } from './getUserGroups';
+export { createJoinRequest } from './createJoinRequest'; 
