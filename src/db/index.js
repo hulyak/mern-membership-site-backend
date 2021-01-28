c421f3fb89c61a2911ba708f308053c268bb905b
hulya <hulyakarakayaa06@gmail.com>
2021-01-27 23:38:51 -0800

    create the accept and reject requests for group and create a new message in the group

--- src/db/index.js
+++ src/db/index.js
@@ -6,3 +6,9 @@ export { createGroup } from './createGroup';
 export { getGroup } from './getGroup';
 export { getMemberPopulatedGroup } from './getMemberPopulatedGroup';
 export { getOwnerPopulatedGroup } from './getOwnerPopulatedGroup';
+export { acceptRequest } from './acceptRequest';
+export { rejectRequest } from './rejectRequest';
+export { getRequestsForGroup } from './getRequestsForGroup';
+export { addMessageToGroup } from './addMessageToGroup';
+export { getMessagesForGroup } from './getMessagesForGroup';
+
